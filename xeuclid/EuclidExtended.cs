ab29c0d694e95fb3bdbe6e4d19600d0b1ddad92a
Lukas Wessel <wessel5991@gmail.com>
2020-12-13 19:44:51 +0100

    Anderen Algorithmus verwendet als zuvor. Der neue sieht etwas nicht-trivialer aus ;)

---
+++ xeuclid/EuclidExtended.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+
+namespace xeuclid
+{
+    public class EuclidExtended
+    {
+        private BigInteger a, b;
+
+        public EuclidExtended()
+        {
+        }
+
+        public EuclidExtended(BigInteger a, BigInteger b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public EuclidExtendedSolution solve()
+        {
+            BigInteger x0 = 1, xn = 1, y0 = 0, yn = 0, x1 = 0, y1 = 1, f, r = a % b;
+
+            while (r > 0)
+            {
+                f = a / b;
+                xn = x0 - f * x1;
+                yn = y0 - f * y1;
+
+                x0 = x1;
+                y0 = y1;
+                x1 = xn;
+                y1 = yn;
+                a = b;
+                b = r;
+                r = a % b;
+            }
+
+            return new EuclidExtendedSolution(xn, yn, b);
+        }
+
+    }
+}
---
+++ xeuclid/EuclidExtendedSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Numerics;
+
+namespace xeuclid
+{
+    public class EuclidExtendedSolution
+    {
+        private BigInteger x, y, d;
+
+        public BigInteger X
+        {
+            get
+            {
+                return this.x;
+            }
+        }
+
+        public BigInteger Y
+        {
+            get
+            {
+                return this.y;
+            }
+        }
+
+        public BigInteger D
+        {
+            get
+            {
+                return this.d;
+            }
+        }
+
+        public EuclidExtendedSolution(BigInteger x, BigInteger y, BigInteger d)
+        {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
+    }
+}
