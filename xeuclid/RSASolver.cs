e2011b8929f6a2f89e3b9db12400e19310cd8648
Lukas Wessel <wessel5991@gmail.com>
2020-12-16 19:07:38 +0100

    Lösung Aufgabe a und b

---
+++ xeuclid/RSASolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xeuclid
+{
+    class RSASolver
+    {
+        // Algorithmus Source:
+        // https://assignmentshark.com/blog/extended-euclidean-algorithm-example/
+        public void Solve(public_key public_Key_Arthur, public_key public_Key_Ford)
+        {
+            BigInteger p = BigInteger.GreatestCommonDivisor(public_Key_Arthur.N, public_Key_Ford.N);
+            BigInteger q1 = public_Key_Arthur.N / p;
+            BigInteger q2 = public_Key_Ford.N / p;
+
+            BigInteger a = public_Key_Arthur.e;
+            BigInteger b = (p - 1) * (q1 - 1);
+
+            EuclidExtended euclidExtended = new EuclidExtended(a, b);
+            EuclidExtendedSolution solution = euclidExtended.solve();
+
+            Console.WriteLine("b): gcd(" + a + " , " + b + ") = {" + solution.D + ", {" + solution.X + ", " + solution.Y + "}}");
+        }
+
+    }
+
+}
---
+++ xeuclid/public_key.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace xeuclid
+{
+    public class public_key
+    {
+        public BigInteger N { get; set; }
+        public BigInteger e { get; set; }
+        public public_key()
+        {
+
+        }
+    }
+}
